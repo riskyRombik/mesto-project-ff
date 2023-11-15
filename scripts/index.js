d51aee070638967a488d8308dd16c7a205efa65b
Roman Gurin <gurin.roman1602@yandex.ru>
2023-11-15 17:14:50 +0300

    Первая версия

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
+const cardPlaces = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: Функция создания карточки
+function createCard(cardTitle, cardImage){
+
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector(".card__image").src = cardImage;
+  cardElement.querySelector(".card__title").textContent = cardTitle;
+  cardElement.querySelector(".card__image").alt = cardTitle;
+
+  cardDeleteButton.addEventListener("click", deleteCard);
+  
+  return cardElement;
+};
+
+// @todo: Функция добавления карточки
+function addCard(card){
+  cardPlaces.append(card);
+};
 
 // @todo: Функция удаления карточки
+function deleteCard(evt){
+  evt.target.parentElement.remove();
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(card) {
+  const cardData = createCard(card.name, card.link);
+  addCard(cardData);
+});
